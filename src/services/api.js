90ecda8f04d917b6fa5fc919895ac9e7c129f114
PietroMartiori <pietromartiori7@gmail.com>
2025-11-21 16:33:53 -0300

    Revert "Create Express"
    This reverts commit 72cc0006685e83a63c6254525a82bdca75eecb5d.

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,1 @@
-import express from "express";
 export const API_URL = "http://localhost:3000/api";
-
-const app = express();
-const PORT = 3000;
-
-app.get('/', (req, res) =>{
-   res.send(`Ola pertile`);
-})
-
-app.listen(PORT, () => console.log(`O servidor esta rodando na porta ${PORT}`));
